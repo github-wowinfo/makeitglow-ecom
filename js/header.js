60d286f2450ec56998122cdb8c455a4c01c9d95a
zeba-a <you@example.com>
2024-01-19 17:55:46 +0530

    add to cart intigration

--- js/header.js
+++ js/header.js
@@ -108,7 +108,6 @@ function calculateSubtotal(cartData) {
 // Whish list api . . . . 
 
 $(document).ready(function () {
-<<<<<<< HEAD
     // Fetch cart data from the API
     $.ajax({
       url: `${SETTINGS.backendUrl}/Ecom/GetWishlistByCustId`,
@@ -124,34 +123,13 @@ $(document).ready(function () {
   
         // Clear existing content
         // $('#shopping-cart-pane .sidebar-cart-list').empty();
+        console.log('cartData', cartData.length);
+      $('#whislistCount').append(cartData.length)
   
         // Update cart items
         cartData.forEach(function (whishlistItem) {
             console.log(whishlistItem);
           var cartItemHTML = `
-=======
-  // Fetch cart data from the API
-  $.ajax({
-    url: `${SETTINGS.backendUrl}/Ecom/GetWishlistByCustId`,
-    method: 'GET',
-    headers: {
-      Authorization: "Bearer " + token,
-      "Content-Type": "application/json",
-      // Add other headers as needed
-    },
-    dataType: 'json',
-    success: function (cartData) {
-      // Assuming cartData is an array of items in the cart
-      console.log('cartData', cartData.length);
-      $('#whislistCount').append(cartData.length)
-      // Clear existing content
-      // $('#shopping-cart-pane .sidebar-cart-list').empty();
-
-      // Update cart items
-      cartData.forEach(function (cartItem) {
-        console.log(cartItem);
-        var cartItemHTML = `
->>>>>>> e7a88b0cec7a9a05c3f5e80f764bd0dd4ac4eff2
                  <li>
                         <div class="cart-widget">
                           <div class="dz-media me-3">
@@ -171,7 +149,6 @@ $(document).ready(function () {
                           </a>
                         </div>
                       </li>`;
-<<<<<<< HEAD
   
           // Append the item to the cart list
           $('#whishlistItem').append(cartItemHTML);
@@ -195,28 +172,3 @@ $(document).ready(function () {
     });
     return subtotal;
   }
-=======
-
-        // Append the item to the cart list
-        $('#cartItem').append(cartItemHTML);
-      });
-
-      // Calculate and update subtotal
-      var subtotal = calculateSubtotal(cartData);
-      $('#shopping-cart-pane .cart-total h5:last-child').text('$' + subtotal.toFixed(2));
-    },
-    error: function (error) {
-      console.error('Error fetching cart data:', error);
-    }
-  });
-});
-
-// Function to calculate subtotal based on cart data
-function calculateSubtotal(cartData) {
-  var subtotal = 0;
-  cartData.forEach(function (cartItem) {
-    subtotal += cartItem.price;
-  });
-  return subtotal;
-}
->>>>>>> e7a88b0cec7a9a05c3f5e80f764bd0dd4ac4eff2
