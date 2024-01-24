bb8855c78545c191428e5827ec73cc8f86ef2310
zeba-a <you@example.com>
2024-01-24 12:27:50 +0530

    add shipping data done

--- js/account.js
+++ js/account.js
@@ -1,3 +1,4 @@
+// .................get profile function start .................. 
 function getProfile() {
     $.ajax({
         url: `${SETTINGS.backendUrl}/Auth/GetProfile`,
@@ -28,6 +29,10 @@ function getProfile() {
     });
 
 }
+// .................get profile function end .................. 
+
+// .................get BillingInfo function start .................. 
+
 function getBillingInfo() {
     $.ajax({
         url: `${SETTINGS.backendUrl}/CustomerAccount/GetAllCustBillingAddresses`,
@@ -67,7 +72,6 @@ function getBillingInfo() {
 
     $.ajax({
         type: "GET",
-        // url: '${SETTINGS.backendUrl}Masters/GetAllLocations',
         url: `${SETTINGS.backendUrl}/Masters/GetAllLocations`,
 
         dataType: "json",
@@ -82,7 +86,6 @@ function getBillingInfo() {
         }
     })
 
-    // Assuming this is your click event handler for the button with id 'saveinfo'
     var savebilling = document.getElementById('saveinfo');
 
     savebilling.addEventListener('click', function (e) {
@@ -90,7 +93,6 @@ function getBillingInfo() {
             addBillingAddress();
     });
 
-    // Function to handle the API call to AddCustBillingAddress
     function addBillingAddress() {
         const CompanyName = document.getElementById("companyName").value;
         const CompanyTaxNo = document.getElementById("companyTaxNo").value;
@@ -110,7 +112,7 @@ function getBillingInfo() {
         };
         console.log('userData :', userData);
         $.ajax({
-            url: 'https://mig-dev.lifelinemegacorp.com/api/CustomerAccount/AddCustBillingAddress',
+            url: `${SETTINGS.backendUrl}/CustomerAccount/AddCustBillingAddress`,
             method: 'POST',  // Assuming this should be a POST request, change it if necessary
             headers: {
                 Authorization: "Bearer " + token,
@@ -119,18 +121,14 @@ function getBillingInfo() {
             },
             data: JSON.stringify(userData), // Convert object to JSON string
             success: function (response) {
-                // Handle success response
-                //    localStorage.setItem('token', response);
-
+           
                 console.log('Billing address added successfully:', response);
                 toastr.success("Billing address added successfully ");
 
-                // You may want to update the UI or perform other actions here
                 $("#saveinfo").modal("hide");
             },
             error: function (error) {
                 console.error('Error adding billing address:', error);
-                // Handle error response
                 toastr.error(error);
 
             }
@@ -138,6 +136,10 @@ function getBillingInfo() {
     }
 
 }
+// .................get BillingInfo function end .................. 
+   
+// .................get shippingInfo function start .................. 
+
 function getshippingInfo() {
     $.ajax({
         url: `${SETTINGS.backendUrl}/CustomerAccount/GetAllCustShippingAddresses`,
@@ -177,10 +179,101 @@ function getshippingInfo() {
         }
     });
 }
+// .................get shippingInfo function end .................. 
+
+// ................. addshippingInfo function start .................. 
+
+function addshippingInfo() {
+   
+    $('#postButton').on('click', function () {
+        $('#shippingModal').modal('show'); // Adjust 'yourModalId' accordingly
+    });
+    $.ajax({
+        type: "GET",
+        // url: '${SETTINGS.backendUrl}Masters/GetAllLocations',
+        url: `${SETTINGS.backendUrl}/Masters/GetAllLocations`,
+
+        dataType: "json",
+        success: function (response) {
+            // console.log(response);
+            let li = `<option value="">Select</option>`
+            $.each(response, function (index, value) {
+                li += `<option value="${value.lEntryId}">${value.locationName}</option>`
+            });
+            $('#countrySelection').append(li);
+            $('#countrySelection').selectric('refresh');
+        }
+    })
+
+    // Assuming this is your click event handler for the button with id 'saveinfo'
+    var saveshipping = document.getElementById('saveshippinginfo');
+
+    saveshipping.addEventListener('click', function (e) {
+        e.preventDefault(),
+            addshippingAddress();
+    });
+
+    // Function to handle the API call to AddCustBillingAddress
+    function addshippingAddress() {
+        const Name = document.getElementById("name").value;
+        const ContactNo = document.getElementById("contactNum").value;
+        const AltContactNo = document.getElementById("altcontactNum").value;
+        const Address1 = document.getElementById("addresses1").value;
+        const Address2 = document.getElementById("addresses2").value;
+        const CountrySelect = document.getElementById("countrySelection").value;
+        const Remark = document.getElementById("remark").value;
+
+        var userData = {
+            "userType": 2,
+            "name": Name,
+            "contactNo": ContactNo,
+            "altContactNo": AltContactNo,
+            "addressLine1": Address1,
+            "addressLine2": Address2,
+            "lctnId": CountrySelect,
+            "remark": Remark,
+        };
+        console.log('userData :', userData);
+        $.ajax({
+            url: 'https://mig-dev.lifelinemegacorp.com/api/CustomerAccount/AddCustShippingAddress',
+            method: 'POST',  // Assuming this should be a POST request, change it if necessary
+            headers: {
+                Authorization: "Bearer " + token,
+                "Content-Type": "application/json",
+                // Add other headers as needed
+            },
+            data: JSON.stringify(userData), // Convert object to JSON string
+            success: function (response) {
+                // Handle success response
+                //    localStorage.setItem('token', response);
+
+                console.log('Billing address added successfully:', response);
+                toastr.success("Billing address added successfully ");
+
+                // You may want to update the UI or perform other actions here
+                $("#saveshippinginfo").modal("hide");
+                location.reload()
+                //  addshippingInfo()
+
+
+            },
+            error: function (error) {
+                console.error('Error adding billing address:', error);
+                // Handle error response
+                toastr.error(error);
+
+            }
+        });
+
+    }
+
+}
+// ................. addshippingInfo function end .................. 
 
 $(document).ready(function () {
     getProfile()
     getBillingInfo()
+    addshippingInfo()
     getshippingInfo()
     // addBillingAddress()
 });
