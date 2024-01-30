f3180c8c095abd171cc28a3c92e9836571187efa
W-nida <nida.w@wowinfobiz.com>
2024-01-30 13:03:56 +0530

    blog detail

---
+++ js/blog.js
@@ -0,0 +1,63 @@
+$.ajax({
+    type: "GET",
+    url: `${SETTINGS.backendUrl}/Blogs/GetAllBlogs`,
+    dataType: "json",
+    success: function (response) {
+        response.forEach(function (blogItem) {
+            console.log('resp', blogItem);
+            var timestampStr = blogItem.creationTime;
+
+            // Parse the timestamp string into a Date object
+            var timestamp = new Date(timestampStr);
+
+            // Format the Date object into the desired format
+            var formattedDate = formatDate(timestamp);
+
+            // Display the formatted date
+            console.log(formattedDate);
+
+            function formatDate(date) {
+                // Array of month names
+                var monthNames = [
+                    "January", "February", "March",
+                    "April", "May", "June", "July",
+                    "August", "September", "October",
+                    "November", "December"
+                ];
+
+                // Extract day, month, and year
+                var day = date.getDate();
+                var monthIndex = date.getMonth();
+                var year = date.getFullYear();
+
+                // Format the date string
+                var formattedDate = day + " " + monthNames[monthIndex] + " " + year;
+
+                return formattedDate;
+            }
+            var blogList = `
+            <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+							<div class="dz-card style-1">
+								<div class="dz-media">
+									<img src="images/blog/blogpost-3/pic1.jpg" alt="/">
+								</div>
+								<div class="dz-info">
+									<div class="dz-meta">
+										<ul>
+											<li class="post-date">${formattedDate}</li>
+										</ul>
+									</div>
+									<h3 class="dz-title">
+										<a href="post-standard.html">${blogItem.headerTitle}</a>
+									</h3>
+									<a href="blogDetail.html?Id=${blogItem.id}" class="font-14 mt-auto read-btn">Read More
+										<i class="icon feather icon-chevron-right"></i>
+									</a>
+								</div>
+							</div>
+						</div>`
+
+            $('#blogListing').append(blogList)
+        })
+    }
+});
