69cc6a1e8359525d15be159ef6ed300232faf14a
W-nida <nida.w@wowinfobiz.com>
2024-02-06 18:28:09 +0530

    registration log

--- js/registration.js
+++ js/registration.js
@@ -354,15 +354,15 @@ document.getElementById('togglePassword').addEventListener('click', function () 
 // }
 
 
-function handleCredentialResponse(response) {
-  // decodeJwtResponse() is a custom function defined by you
-  // to decode the credential response.
-  const responsePayload = decodeJwtResponse(response.credential);
+// function handleCredentialResponse(response) {
+//   // decodeJwtResponse() is a custom function defined by you
+//   // to decode the credential response.
+//   const responsePayload = decodeJwtResponse(response.credential);
 
-  console.log("ID: " + responsePayload.sub);
-  console.log('Full Name: ' + responsePayload.name);
-  console.log('Given Name: ' + responsePayload.given_name);
-  console.log('Family Name: ' + responsePayload.family_name);
-  console.log("Image URL: " + responsePayload.picture);
-  console.log("Email: " + responsePayload.email);
-}
+//   console.log("ID: " + responsePayload.sub);
+//   console.log('Full Name: ' + responsePayload.name);
+//   console.log('Given Name: ' + responsePayload.given_name);
+//   console.log('Family Name: ' + responsePayload.family_name);
+//   console.log("Image URL: " + responsePayload.picture);
+//   console.log("Email: " + responsePayload.email);
+// }
